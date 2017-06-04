c4e37c4a537f29bdd36a1eeb33d3cede369cfafd
AngryCracker <ads6may@gmail.com>
2017-06-04 17:23:01 +0530

    expanded dataframe

--- regression.py
+++ regression.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import quandl
+import math
 
 quandl.ApiConfig.api_key = 'VDxfKkzAm8MFL1fZsSat'
 df = quandl.get_table('WIKI/PRICES')
@@ -7,5 +8,22 @@ df = quandl.get_table('WIKI/PRICES')
 df = df[['adj_open', 'adj_high', 'adj_low', 'adj_close', 'adj_volume']]
 df['hl_pct'] = (df['adj_high'] - df['adj_close']) / df['adj_close'] * 100
 df['pct_change'] = (df['adj_close'] - df['adj_open']) / df['adj_open'] * 100
+# The ones below are features
 df = df[['adj_close', 'hl_pct', 'pct_change', 'adj_volume']]
+# print(df.head())
+
+forecast_col = 'adj_close'
+# fills specified value wherever 'fill' is not available
+# In machine learning, we don't want to have null data. We could remove the 
+# entire column, but that would lead to 'wastage' of data in the other attributes
+# An outlandish value in place of the unavailable data is more or less regarded as an 
+# outlying value and will be 'ignored'
+# this is better than sacrificing data
+df.fillna(-99999, inplace=True)
+
+# here we are trying to predict out 10% of the dataframe
+forecast_out = int(math.ceil(0.1*len(df)))
+
+# we are basically creating a space ten% days out into the future, thus the negative shift
+df['label'] = df[forecast_col].shift(-forecast_out)
 print(df.head())
