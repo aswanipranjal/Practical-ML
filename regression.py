62134245f6e4a3c487bf868419ca50f0f91ceb86
AngryCracker <ads6may@gmail.com>
2017-06-04 17:54:01 +0530

    Added sklearn training and testing code. Obviously, it didn't work

--- regression.py
+++ regression.py
@@ -46,4 +46,11 @@ X = preprocessing.scale(X)
 df.dropna(inplace = True)
 y = np.array(df['label'])
 
-print(len(X), len(y))
+# print(len(X), len(y))
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
