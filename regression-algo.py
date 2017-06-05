d77379be16f55ec2536f1770d2e8080145c9d1da
AngryCracker <ads6may@gmail.com>
2017-06-05 22:01:09 +0530

    Calculated coefficient of determination

--- regression-algo.py
+++ regression-algo.py
@@ -17,6 +17,15 @@ def regress(xs, ys):
 	b = mean(ys) - m*mean(xs)
 	return m, b
 
+def squared_error(ys_original, ys_line):
+	return sum((ys_line - ys_original)**2)
+
+def coefficient_of_determination(ys_original, ys_line):
+	y_mean_line = [mean(ys_original) for y in ys_original]
+	squared_error_regression_line = squared_error(ys_original, ys_line)
+	squared_error_mean_line = squared_error(ys_original, y_mean_line)
+	return 1 - (squared_error_regression_line/squared_error_mean_line)
+
 m, b = regress(xs, ys)
 print(m, b)
 
@@ -24,7 +33,15 @@ regression_line = [(m*x)+b for x in xs]
 
 predict_x = 8
 predict_y = (m*predict_x) + b
+
+r_squared = coefficient_of_determination(ys, regression_line)
+print(r_squared)
+
 plt.scatter(xs, ys)
 plt.scatter(predict_x, predict_y, color='g')
 plt.plot(xs, regression_line)
 plt.show();
+# to find how accurate the best fit line is
+# Squared error : SE
+# Coefficient of determination R^2 = 1 - (SE of y-hat)/(SE of mean)
+# Basically 1 minus the ratio of how accurate the best fit line is compared to the mean line
