949c79a34d83874aaccbd767d52abe04209a9889
AngryCracker <ads6may@gmail.com>
2017-06-06 00:36:35 +0530

    Visualizing dataset

--- knn-algo.py
+++ knn-algo.py
@@ -17,3 +17,16 @@ style.use('fivethirtyeight')
 # features that correspond to the class of 'k'
 # class 'r' has labels
 dataset = {'k':[[1, 2], [2, 3], [3, 1]], 'r':[[6, 5], [7, 7], [8, 6]]}
+new_features = [5, 7]
+
+# i corresponds to 'k' and 'r'
+# ii corresponds to each feature (a pair of coordinates)
+
+# The nested for loop below can be written as the one-liner even below
+# for i in dataset:
+	# for ii in dataset[i]:
+		# plt.scatter(ii[0], ii[1], s=100, color=i)
+
+[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
+plt.scatter(new_features[0], new_features[1])
+plt.show()
