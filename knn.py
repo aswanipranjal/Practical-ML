2a63173bfce21eb40e6db8b9baf1c9e16c7da966
AngryCracker <ads6may@gmail.com>
2017-06-05 23:53:30 +0530

    Split datasets

--- knn.py
+++ knn.py
@@ -8,3 +8,11 @@ df = pd.read_csv('cancer_dataset.txt')
 df.replace('?', -99999, inplace=True)
 # KNN handles outliers very badly, so we need to be careful
 df.drop(['id'], 1, inplace=True)
+# to drop entries with missing data, we write df.dropna(inplace=True)
+
+# df.drop(['_attribute_'], 1) basically returns a dataframe without the attribute column in it and can be converted into an array by numpy
+X = np.array(df.drop(['class'], 1))
+y = np.array(df['class'])
+
+# the line below separates training and testing data into four arrays according to the percentage of data we specify
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
