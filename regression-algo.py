59d31ff4312e5596920f638a27cd90fa9377d40e
AngryCracker <ads6may@gmail.com>
2017-06-05 21:33:00 +0530

    Intercept found

--- regression-algo.py
+++ regression-algo.py
@@ -9,9 +9,10 @@ ys = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
 # plt.scatter(xs, ys)
 # plt.show()
 
-def best_fit_slope(xs, ys):
+def regress(xs, ys):
 	m = (((mean(xs)*mean(ys)) - (mean(xs*ys))) / (((mean(xs))**2) - mean(xs**2)))
-	return m
+	b = mean(ys) - m*mean(xs)
+	return m, b
 
-m = best_fit_slope(xs, ys)
-print(m)
+m, b = regress(xs, ys)
+print(m, b)
