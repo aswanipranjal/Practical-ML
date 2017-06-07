8f8ac3d2d772c8e4dcfaf623d1647a2021240fda
AngryCracker <ads6may@gmail.com>
2017-06-07 14:11:33 +0530

    changed colors

--- svm-algo.py
+++ svm-algo.py
@@ -100,19 +100,19 @@ class Support_Vector_Machine:
 		# positive support vector hyperplane
 		psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
 		psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
-		self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2])
+		self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2], 'k')
 
 		# (w.x + b) = -1
 		# negative support vector hyperplane
 		nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
 		nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
-		self.ax.plot([hyp_x_min, hyp_x_max], [nsv1, nsv2])
+		self.ax.plot([hyp_x_min, hyp_x_max], [nsv1, nsv2], 'k')
 
 		# (w.x + b) = 0
 		# positive support vector hyperplane
 		db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
 		db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
-		self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2])
+		self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2], 'y--')
 
 		plt.show()
 
