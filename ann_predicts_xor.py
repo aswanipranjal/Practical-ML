704816793ec563cd2b0e9b440896f712f00cc415
AngryCracker <ads6may@gmail.com>
2017-06-07 15:00:28 +0530

    Making an ANN to predict XOR values

---
+++ ann_predicts_xor.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def nonlin(x, deriv=False):
+	if deriv:
+		return x*(1-x)
+
+	return 1/(1 + np.exp(-x))
+
+# input data
+X = np.array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
+# output data
+y = np.array([[0], [1], [1], [0]])
+
+np.random.seed(1)
+
+# synapses
+syn0 = 2*np.random.random((3, 4)) - 1
+syn1 = 2*np.random.random((4, 1)) - 1
+
+# training step
+for j in range(60000):
+	l0 = X
+	l1 = nonlin(np.dot(l0, syn0))
+	l2 = nonlin(np.dot(l1, syn1))
+
+	l2_error = y - 12
+
+	if (j % 100000) == 0:
+		print "Error: " + str(np.mean(np.abs(l2_error)))
+
+	l2_delta = l2_error*nonlin(l2, deriv=True)
