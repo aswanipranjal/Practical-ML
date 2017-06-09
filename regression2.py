e037d160896776146f7f65eb0c2cb21b2d07712d
AngryCracker <ads6may@gmail.com>
2017-06-09 22:37:40 +0530

    Computed error

--- regression2.py
+++ regression2.py
@@ -1,6 +1,25 @@
 from numpy import *
 
-def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations)
+def compute_error_for_given_points(b, m, points):
+	totalError = 0
+	for i in range(len(points)):
+		x = points[i, 0]
+		y = points[i, 1]
+		totalError += (y - (m * x + b))**2
+	# we divide the sum by the total number of elements in the array
+	return totalError / float(len(points))
+
+def step_gradient(b_current, m_current, points, learning_rate):
+	# Gradient Descent
+
+def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+	b = starting_b
+	m = starting_m
+
+	for i in range(num_iterations):
+		b, m = step_gradient(b, m, array(points), learning_rate)
+
+	return [b, m]
 
 def run():
 	# read dataset
