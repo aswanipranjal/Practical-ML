df4f811a119dab29ede1558f4a310b68044efea3
AngryCracker <ads6may@gmail.com>
2017-06-09 23:06:43 +0530

    minor

--- regression2.py
+++ regression2.py
@@ -38,7 +38,7 @@ def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_i
 def run():
 	# read dataset
 	# points has [x, y] value pairs
-	points = genfromtext('student_data.csv', delimiter=',')
+	points = genfromtxt('C:\\Users\\Aman Deep Singh\\Documents\\Python\\Practical ML\\student_data.csv', delimiter=',')
 	# hyperparameters
 	learning_rate = 0.0001
 	# y = mx + b
@@ -48,8 +48,8 @@ def run():
 	# we have a pretty small dataset, so we will iterate for 1000 cycles
 	num_iterations = 1000
 	[b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
-	print(b)
-	print(m)
+	print("The optimal intercept is: ", b)
+	print("The optimal slope is: ", m)
 
-if __name__ = '__main__':
+if __name__ == '__main__':
 	run()
