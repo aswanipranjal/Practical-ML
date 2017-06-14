641ea413f910e96dff4da660a0c014c5fc00f6a0
AngryCracker <ads6may@gmail.com>
2017-06-14 23:35:50 +0530

    Computed cost

--- machine-learning-ex1/ex1/plotData.m
+++ machine-learning-ex1/ex1/plotData.m
@@ -18,7 +18,9 @@ figure; % open a new figure window
 
 
 
-
+plot(x, y, 'rx', 'MarkerSize', 10);
+xlabel('Population of City in 10,000s');
+ylabel('Profit in $10,000s');
 
 % ============================================================
 
