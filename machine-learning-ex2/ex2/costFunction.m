ac1c80df4e87ab1b309ea1b2f181c89b5ed030f4
AngryCracker <ads6may@gmail.com>
2017-06-17 13:22:11 +0530

    Added costFunction.m

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -19,12 +19,9 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
-H = sigmoid(X);
-logh = log(H);
-logmh = log(1-H);
-J = (1/m)*((-logh'*y) - logmh'*(1-y));
-
-grad = (1/m)*()
+H = sigmoid(X * theta);
+J = 1/m*(-log(H)'*y - log(1 - H)'*(1-y));
+grad = 1/m*(X' * (H - y));
 
 % =============================================================
 
