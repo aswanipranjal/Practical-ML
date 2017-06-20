6ef497ed99e44c37a8d9c87421ef0538fb51a820
AngryCracker <ads6may@gmail.com>
2017-06-20 15:56:22 +0530

    another very basic ann

---
+++ ann.py
@@ -0,0 +1,20 @@
+if __name__ = '__main__':
+	# Initialize a single neuron neural network
+	neural_network = NeuralNetwork()
+	print ('Random starting synaptic weights:')
+	print (neural_network.synaptic_weights)
+
+	# The training set. We have 4 examples, each consisting of 3 input values and 1 output value
+	training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+	training_set_outputs = array([[0, 1, 1, 0]]).T # The T function returns the transpose of the output set
+
+	# train the neural netwrok using the training set.
+	# Do it 10,000 times and make small adjustments each time
+	neural_network.train(training_set_inputs, training_set_outputs, 10000)
+
+	print ('New synaptic weights after training: ')
+	print (neural_network.synaptic_weights)
+
+	# Test the neural network
+	print ('Predicting')
+	print (neural_network.predict(array([1, 0, 0])))
