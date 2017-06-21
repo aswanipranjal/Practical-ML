a55af77127cffdeecc6b657caf806ebece59775d
AngryCracker <ads6may@gmail.com>
2017-06-21 18:01:22 +0530

    Plotting works

--- GAmine/linearregression.m
+++ GAmine/linearregression.m
@@ -7,8 +7,17 @@ load('X.mat');
 load('y.mat');
 % size(y)
 fprintf('Matrices loaded\n');
-X = X(:, 2:end);
+X = X(:, 2:end); % separating the useful part of the data as the first column is just the car number
 % plot y data here to see if things are loading
 % plotData(y);
-y = y(:, 2:end);
+y = y(:, 2:end); % separating the useful part of the data as the first column is just the car number
 % fprintf('Split linear regression training data\n');
+% Implement linear regresion function for the first column of X only
+m = length(y);
+X = X(:, 1);
+% plotData(X, y);
+X = [ones(m, 1) X];
+theta = zeros(2, 1); % [2x1]
+iterations = 1500;
+alpha = 0.01;
+fprintf('\nTesting the cost function\n');
--- GAmine/plotData.m
+++ GAmine/plotData.m
@@ -1,6 +1,6 @@
-function plotData(y)
+function plotData(X, y)
 	figure; % opens a new figure window
-	plot(y(:, 1), y(:, 2), 'k-', 'MarkerSize', 5);
+	plot(X, y, 'kx', 'MarkerSize', 5);
 	xlabel('Car number');
 	ylabel('Track record time');
 end;
