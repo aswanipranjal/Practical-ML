f2d26687aa4c6aa6d235eb8335640ad458dcf25a
AngryCracker <ads6may@gmail.com>
2017-06-21 18:31:30 +0530

    trying to plot stuff

--- GAmine/linearregression.m
+++ GAmine/linearregression.m
@@ -38,3 +38,39 @@ hold on;
 plot(X(:, 2), X*theta, '-')
 legend('Training data', 'Linear Regression');
 hold off;
+
+predict1 = [1, 0.8]*theta;
+fprintf('For a max steering angle of 0.8(normalized), we predict a track record time of %f\n', predict1);
+predict2 = [1, 0.5]*theta;
+fprintf('For a max steering angle of 0.5(normalized), we predict a track record time of %f\n', predict2);
+fprintf('Program paused. Press enter to continue.\n');
+
+fprintf('Visualizing J(theta_0, theta_1)\n');
+
+theta0_vals = linspace(-10, 10, 100);
+theta1_vals = linspace(-1, 4, 100);
+% I couldn't guess what the third argument does.
+J_vals = zeros(length(theta0_vals), length(theta1_vals));
+
+% fill out J_vals
+for i = 1:length(theta0_vals)
+	for j = 1:length(theta1_vals)
+		t = [theta0_vals(i); theta1_vals(j)];
+		J_vals(i, j) = computeCost(X, y, t);
+	end;
+end;
+
+J_vals = J_vals';
+
+% Surface plot
+figure;
+surf(theta0_vals, theta1_vals, J_vals);
+xlabel('\theta0'); ylabel('\theta1');
+
+% Contour plot
+figure;
+% Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
+contour(theta0_vals, theta1_vals, J_vals, logspace(-10, 10, 20))
+xlabel('\theta0'); ylabel('\theta1');
+hold on;
+plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
