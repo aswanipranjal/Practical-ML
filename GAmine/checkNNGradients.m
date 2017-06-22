a1a0e9eb2ce78b1f21809e2fc086031b03b17b59
AngryCracker <ads6may@gmail.com>
2017-06-22 19:34:41 +0530

    numerically checking gradients

--- GAmine/checkNNGradients.m
+++ GAmine/checkNNGradients.m
@@ -10,4 +10,31 @@ function checkNNGradients(lambda)
 	m = 5;
 
 	% We generate some 'random' test-data
-	Theta1 
+	Theta1 = debugInitializeWeights(hidden_layer_size, input_layer_size);
+	Theta2 = debugInitializeWeights(num_labels, hidden_layer_size);
+
+	X = debugInitializeWeights(m, input_layer_size - 1);
+	y = 1 + mod(1:m, num_labels)';
+
+	% Unroll parameters
+	nn_params = [Theta1(:) ; Theta2(:)];
+
+	% Short-hand for cost function
+	costFunc = @(p) nnCostFunction(p, input_layer_size, hidden_layer_size, num_labels, X, y, lambda);
+
+	[cost, grad] = costFunc(nn_params);
+	numgrad = computeNumericalGradient(costFunc, nn_params);
+
+	% Visually examine the two gradient computations. the two columns we get should be very similar
+	disp([numgrad grad]);
+	fprintf(['The above two columns you get should be very similar.\n' '(Left-Your Numerical Gradient, Right Analytical Gradient)\n\n']);
+
+	% evaluate the norm of the difference between the two solutions.
+	% If you have a correct implementation, and assuming you used EPSILON = 0.0001
+	% in computeNumericalGradient.m, then the diff below should be less than 1e-9
+	diff = norm(numgrad - grad)/norm(numgrad + grad);
+
+	fprintf(['If backpropagation implementation is correct, then \n' ...
+			 'the relative difference will be small (less than 1e-9). \n' ...
+			 '\nRelative Difference: %g\n'], diff);
+end;
