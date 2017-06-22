04c9baba81bb1eefaa780551657c627a23e85988
AngryCracker <ads6may@gmail.com>
2017-06-22 18:07:57 +0530

    cannot visualize the datasets. Use python scripts

--- GAmine/neuralnetwork.m
+++ GAmine/neuralnetwork.m
@@ -15,3 +15,20 @@
 % 100001 - 110000 ->[0 0 0 0 0 0 0 0 0 0 1 0]
 % >110000 -> 		[0 0 0 0 0 0 0 0 0 0 0 1]
 % Todo: convert all labels into one-hot vectors
+
+% Initialization
+clear; close all; clc
+input_layer_size = 18; % 18 genetically alterable parameters
+hidden_layer_size = 25; % 25 hidden units
+num_labels = 12; % 12 labels, from 0 - 120000 in multiples of 10000
+
+% Loading and visualizing the datasets
+fprintf('Loading datasets\n');
+load('data_80.mat');
+% size(X)
+load('converted_fitness_80.mat');
+% size(y)
+m = size(X, 1);
+fprintf('Loaded datasets\n');
+fprintf('Program has been paused. Press enter to continue\n');
+pause;
