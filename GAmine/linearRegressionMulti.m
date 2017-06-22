6de2967397b82b1bf518b8af4915bfa44bd6f7e2
AngryCracker <ads6may@gmail.com>
2017-06-22 14:37:28 +0530

    testing test data set

--- GAmine/linearRegressionMulti.m
+++ GAmine/linearRegressionMulti.m
@@ -24,7 +24,7 @@ fprintf('Added a column of ones\n');
 fprintf('Running gradient descent\n');
 % Setting hyperparameters
 alpha = 0.01;
-num_iters = 400;
+num_iters = 10000;
 theta = zeros(19, 1);
 % size(theta)
 % The computCost function and the computeGradient functions written in the previous linear regression example were well vectorized, so we can directly use them.
@@ -36,7 +36,7 @@ plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
 
-% Displaye gradient descent's result:
+% Display gradient descent's result:
 fprintf('Theta computed from gradient descent: \n');
 fprintf('%f\n', theta);
 fprintf('\n');
@@ -44,8 +44,19 @@ fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 fprintf('Predicting stuff\n');
-x_test = [1,0.77824,0.05768192,0.8901123,0.6041341,0.1500494,0.5331748,0.9752828,0.7247745,0.02214646,0.3577529,0.0419383,0.7006501,0.5528352,0.8018743,0.8233464,0.8771472,0.6299444,0.09685636];
-y_test = [35325];
+x_t = [1,0.77824,0.05768192,0.8901123,0.6041341,0.1500494,0.5331748,0.9752828,0.7247745,0.02214646,0.3577529,0.0419383,0.7006501,0.5528352,0.8018743,0.8233464,0.8771472,0.6299444,0.09685636];
+y_t = [35325];
+h_t = x_t*theta;
+fprintf('Predicted track time: %f\n', h_t);
+fprintf('Expected value: %f\n', y_t);
+
+fprintf('Predicting stuff of test data set\n');
+x_test = load('data.txt');
+y_test = load('fitness.txt');
+% Dropping the first column
+x_test = x_test(:, 2:end);
+y_test = y_test(:, 2:end);
+x_test = [ones(size(x_test, 1), 1) x_test];
 h_test = x_test*theta;
-fprintf('Predicted track time: %f\n', h_test);
-fprintf('Expected value: %f\n', y_test);
+fprintf('Predicted		Expected\n');
+[h_test y_test]
