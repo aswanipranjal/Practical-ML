bafe3f854dc4f2cf834d74086c70e05c2747646c
AngryCracker <ads6may@gmail.com>
2017-06-23 17:15:50 +0530

    bugfixes

--- deepnet.py
+++ deepnet.py
@@ -24,11 +24,11 @@ y = tf.placeholder('float')
 def neural_network(data):
     # (input_data * weights) + biases
     hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])), 
-                      'biases':tf.Variable(tf.random_normal(n_nodes_hl1))}
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
     hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])), 
-                      'biases':tf.Variable(tf.random_normal(n_nodes_hl2))}
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
     hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])), 
-                      'biases':tf.Variable(tf.random_normal(n_nodes_hl3))}
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
     output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])), 
                       'biases':tf.Variable(tf.random_normal([n_classes]))}
     
@@ -56,11 +56,11 @@ def train_neural_network(x):
     with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
         
-        for epoch in hm_epochs:
+        for epoch in range(hm_epochs):
             epoch_loss = 0
             for _ in range(int(mnist.train.num_examples/batch_size)):
-                x, y = mnist.train.next_batch(batch_size)
-                _, c = sess.run([optimizer, cost], feed_dict={x: x, y: y})
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
                 epoch_loss += c
             print('Epoch ', epoch, ' completed out of ', hm_epochs, 'loss: ', epoch_loss)
             
