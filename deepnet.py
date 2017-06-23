d530d6876ca8208afad6f4a44b3b4a0f45065c76
AngryCracker <ads6may@gmail.com>
2017-06-23 17:59:11 +0530

    more tweaks

--- deepnet.py
+++ deepnet.py
@@ -24,14 +24,26 @@ y = tf.placeholder('float')
 def neural_network(data):
     # (input_data * weights) + biases
     hidden_1_layer = {'weights':tf.Variable(tf.truncated_normal([784, n_nodes_hl1], stddev=0.1)), 
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+                      'biases':tf.Variable(tf.constant(0.1, shape=[n_nodes_hl1]))}
     hidden_2_layer = {'weights':tf.Variable(tf.truncated_normal([n_nodes_hl1, n_nodes_hl2], stddev=0.1)), 
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
+                      'biases':tf.Variable(tf.constant(0.1, shape=[n_nodes_hl2]))}
     hidden_3_layer = {'weights':tf.Variable(tf.truncated_normal([n_nodes_hl2, n_nodes_hl3], stddev=0.1)), 
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
+                      'biases':tf.Variable(tf.constant(0.1, shape=[n_nodes_hl3]))}
     output_layer = {'weights':tf.Variable(tf.truncated_normal([n_nodes_hl3, n_classes], stddev=0.1)), 
-                      'biases':tf.Variable(tf.random_normal([n_classes]))}
+                      'biases':tf.Variable(tf.constant(0.1, shape=[n_classes]))}
     
+    # Having a small positive initial bias means the neurons are more likely to fire in average. 
+    # That speeds up training because backpropagation relies on how much impact each neuron has on the output, 
+    # and inactive neurons have no impact whatsoever. It doesn't really matter if they're all the same, 
+    # since the bias just controls the activation threshold, so we set it to a constant.
+    # Now, having a large amplitude for the weights makes the network more unstable, and thus harder to train, 
+    # since each learning step may change the functional behavior a lot. So we start those values randomly (like sentdex did), 
+    # but with a smaller standard deviation (0.1) and without any values larger than 0.2 in magnitude 
+    # (which is what the truncated normal distribution does). That ensures we get a better initial condition, 
+    # so training goes smoother.
+    # There's some work out there about how to pick a good initialization, but people often just try a bunch of variations 
+    # from what I suggested until they find what works best.
+
     l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
     # ReLURectified linear something. A low-cost activation function.
     l1 = tf.nn.relu(l1)
@@ -51,7 +63,7 @@ def train_neural_network(x):
     # learning rate = 0.001
     optimizer = tf.train.AdamOptimizer().minimize(cost)
     # cycles of feedforward and backprop
-    hm_epochs = 10
+    hm_epochs = 15
     
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
