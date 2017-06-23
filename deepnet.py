6339e2188ad4ecfcee07524495de06136c668937
AngryCracker <ads6may@gmail.com>
2017-06-23 17:56:04 +0530

    performance tweak

--- deepnet.py
+++ deepnet.py
@@ -23,13 +23,13 @@ y = tf.placeholder('float')
 
 def neural_network(data):
     # (input_data * weights) + biases
-    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])), 
+    hidden_1_layer = {'weights':tf.Variable(tf.truncated_normal([784, n_nodes_hl1], stddev=0.1)), 
                       'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
-    hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])), 
+    hidden_2_layer = {'weights':tf.Variable(tf.truncated_normal([n_nodes_hl1, n_nodes_hl2], stddev=0.1)), 
                       'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
-    hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])), 
+    hidden_3_layer = {'weights':tf.Variable(tf.truncated_normal([n_nodes_hl2, n_nodes_hl3], stddev=0.1)), 
                       'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
-    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])), 
+    output_layer = {'weights':tf.Variable(tf.truncated_normal([n_nodes_hl3, n_classes], stddev=0.1)), 
                       'biases':tf.Variable(tf.random_normal([n_classes]))}
     
     l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
