7413a4129957216bddcae90c816325e91013844e
AngryCracker <ads6may@gmail.com>
2017-06-24 20:06:22 +0530

    plotting data

--- tf_basic2.py
+++ tf_basic2.py
@@ -2,3 +2,36 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 from sklearn.metrics import confusion_matrix
+from tensorflow.examples.tutorials.mnist import input_data
+data = input_data.read_data_sets("tmp/data", one_hot=True)
+print("Size of: ")
+print("-Training set: \t\t{}".format(len(data.train.labels)))
+print("-Test set: \t\t{}".format(len(data.test.labels)))
+print("-Validation set: \t{}".format(len(data.validation.labels)))
+img_size = 28
+img_size_flat = img_size * img_size
+img_shape = (img_size, img_size)
+num_classes = 10
+
+def plot_images(images, cls_true, cls_pred=None):
+	assert len(images) == len(cls_true) == 9
+
+	# Create figure with 3x3 subplots
+	fig, axes = plt.subplots(3, 3)
+	fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
+	for i, ax in enumerate(axes.flat):
+		# Plot image
+		ax.imshow(images[i].reshape(img_shape), cmap='binary')
+
+		# Show true and predicted classes
+		if cls_pred is None:
+			xlabel = "True: {0}".format(cls_true[i])
+		else:
+			xlabel = "True: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
+
+		ax.set_xlabel(xlabel)
+
+		# Remove ticks from the plot
+		ax.set_xticks([])
+		ax.set_yticks([])
