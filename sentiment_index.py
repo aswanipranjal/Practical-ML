6ec3c0c9cdaf6a4708f2cf92bc95b29900f53e96
AngryCracker <ads6may@gmail.com>
2017-06-24 12:58:35 +0530

    tokenizers not found

--- sentiment_index.py
+++ sentiment_index.py
@@ -49,6 +49,20 @@ def sample_handling(sample, lexicon, classification):
 def create_feature_sets_and_labels(pos, neg, test_size=0.1):
 	lexicon = create_lexicon(pos, neg)
 	features = []
-	features += sample_handling('C:/Users/Aman Deep Singh/Documents/Python/Practical ML/pos.txt', lexicon, [1, 0])
-	features += sample_handling('C:/Users/Aman Deep Singh/Documents/Python/Practical ML/neg.txt', lexicon, [0, 1])
+	features += sample_handling('C:\\Users\\Aman Deep Singh\\Documents\\Python\\Practical ML\\pos.txt', lexicon, [1, 0])
+	features += sample_handling('C:\\Users\\Aman Deep Singh\\Documents\\Python\\Practical ML\\neg.txt', lexicon, [0, 1])
 	random.shuffle(features)
+
+	features = np.array(features)
+	testing_size = int(test_size*len(features))
+	train_x = list(features[:, 0][:-testing_size])
+	train_y = list(features[:, 1][:-testing_size])
+	test_x = list(features[:, 0][-testing_size:])
+	test_y = list(features[:, 1][-testing_size:])
+
+	return train_x, train_y, test_x, test_y
+
+if __name__ == '__main__':
+	train_x, train_y, test_x, test_y = create_feature_sets_and_labels('C:/Users/Aman Deep Singh/Documents/Python/Practical ML/pos.txt', 'C:/Users/Aman Deep Singh/Documents/Python/Practical ML/neg.txt')
+	with open('sentiment_set.pickle', 'wb') as f:
+		pickle.dump([train_x, train_y, test_x, test_y], f)
