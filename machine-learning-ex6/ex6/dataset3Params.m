25b2eee872a431823a10ddb27f7192c088b5fb81
AngryCracker <ads6may@gmail.com>
2017-06-27 03:15:14 +0530

    testing hyper-parameters

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -10,6 +10,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 % You need to return the following variables correctly.
 C = 1;
 sigma = 0.3;
+limits = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -22,12 +23,27 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+minimum_error = Inf;
+min_C = Inf;
+min_sigma = Inf;
 
+for i = 1:8,
+	C = limits(i);
+	for j = 1:8,
+		sigma = limits(j);
+		model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+		predictions = svmPredict(model, Xval);
+		prediction_error = mean(double(predictions ~= yval));
+		if prediction_error < minimum_error,
+			minimum_error = prediction_error;
+			min_C = C;
+			min_sigma = sigma;
+		end;
+	end;
+end;
 
-
-
-
-
+C = min_C;
+sigma = min_sigma;
 
 % =========================================================================
 
