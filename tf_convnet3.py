1766e650c1a2373efd27a33e20927d0b9a23c004
AngryCracker <ads6may@gmail.com>
2017-06-26 18:26:10 +0530

    Added function for definition of convolutional neural network

--- tf_convnet3.py
+++ tf_convnet3.py
@@ -58,6 +58,14 @@ def flatten_layer(layer):
 	layer_flat = tf.reshape(layer, [-1, num_features])
 	return layer_flat, num_features
 
+def new_fc_layer(input, num_inputs, num_outputs, use_relu=True):
+	weights = new_weights(shape=[num_inputs, num_outputs])
+	biases = new_biases(length=num_outputs)
+	layer = tf.matmul(input, weights) + biases
+	if use_relu:
+		layer = tf.nn.relu(layer)
+	return layer
+
 # function to define the convolutional neural network
 def define_cnn(x_image=None, num_channels=None, filter_size1=None, num_filters1=None, filter_size2=None, num_filters2=None, fc_size=None, num_classes=None):
 	# Convolutional layer 1
@@ -68,4 +76,23 @@ def define_cnn(x_image=None, num_channels=None, filter_size1=None, num_filters1=
 	layer_conv2, weights_conv2 = new_conv_layer(input=layer_conv1, num_input_channels=num_filters1, filter_size=filter_size2, num_filters=num_filters2, use_pooling=True)
 	print(layer_conv2.shape)
 
-	layer_flat, num_features = 
+	layer_flat, num_features = flatten_layer(layer_conv2)
+	print(layer_flat.shape)
+	print(num_features) # 1764
+
+	# Fully connected layer 1
+	layer_fc1 = new_fc_layer(input=layer_flat, num_inputs=num_features, num_outputs=fc_size, use_relu=True)
+	print(layer_fc1.shape)
+
+	# Fully connected layer 2
+	layer_fc2 = new_fc_layer(input=layer_fc1, num_inputs=fc_size, num_outputs=num_classes, use_relu=False)
+	print(layer_fc2.shape)
+
+	# Predicted class
+	y_pred = tf.nn.softmax(layer_fc2)
+	y_pred_cls = tf.nn.argmax(y_pred, dimension=1)
+
+	# Cost function to be optimimzed
+	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=layer_fc2, labels=y_true))
+
+	return y_pred, y_pred_cls, loss, weights_conv1, weights_conv2
