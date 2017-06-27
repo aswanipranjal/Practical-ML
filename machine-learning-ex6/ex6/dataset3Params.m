1e043b96fb8aeb2344a8ba2a195d36634a3d2734
AngryCracker <ads6may@gmail.com>
2017-06-27 12:54:41 +0530

    minor

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -25,27 +25,27 @@ limits = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 %
 
 % All the code below works fine. Commented it out so that submission takes less time
-% minimum_error = Inf;
-% min_C = Inf;
-% min_sigma = Inf;
+minimum_error = Inf;
+min_C = Inf;
+min_sigma = Inf;
 
-% for i = 1:8,
-% 	C = limits(i);
-% 	for j = 1:8,
-% 		sigma = limits(j);
-% 		model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
-% 		predictions = svmPredict(model, Xval);
-% 		prediction_error = mean(double(predictions ~= yval));
-% 		if prediction_error < minimum_error,
-% 			minimum_error = prediction_error;
-% 			min_C = C;
-% 			min_sigma = sigma;
-% 		end;
-% 	end;
-% end;
+for i = 1:8,
+	C = limits(i);
+	for j = 1:8,
+		sigma = limits(j);
+		model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+		predictions = svmPredict(model, Xval);
+		prediction_error = mean(double(predictions ~= yval));
+		if prediction_error < minimum_error,
+			minimum_error = prediction_error;
+			min_C = C;
+			min_sigma = sigma;
+		end;
+	end;
+end;
 
-% C = min_C;
-% sigma = min_sigma;
+C = min_C;
+sigma = min_sigma;
 
 % =========================================================================
 
