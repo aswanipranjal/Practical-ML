7e5f58e60b1186d59ccc90d4deb2b631a9732645
AngryCracker <ads6may@gmail.com>
2017-06-28 17:33:03 +0530

    Convolutional neural network ensemble

---
+++ convnet_ensemble.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from datetime import timedelta
+import time
+import math
+import os
+import prettytensor
