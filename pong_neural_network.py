6b391cf2e2264241f1ee9f7b9d0f70db2b8ba9c4
AngryCracker <ads6may@gmail.com>
2017-06-28 12:11:59 +0530

    Building pong

---
+++ pong_neural_network.py
@@ -0,0 +1,18 @@
+# Deep Q-learning (reinforcement learning)
+import pygame
+import random
+
+fps = 60
+window_height = 400
+window_width = 400
+
+paddle_width = 10
+paddle_height = 60
+
+ball_width = 10
+ball_height = 10
+
+paddle_speed = 2
+ball_x_speed = 3
+ball_y_speed = 2
+
