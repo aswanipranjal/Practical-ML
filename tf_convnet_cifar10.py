5ef3d8a72a834c2c4ae42370236d6fed8ad5b047
AngryCracker <ads6may@gmail.com>
2017-07-01 01:46:09 +0530

    Defining variables

--- tf_convnet_cifar10.py
+++ tf_convnet_cifar10.py
@@ -16,3 +16,19 @@ print("PrettyTensor version: {}".format(pt.__version__))
 import cifar10
 # A 163 MB download
 cifar10.data_path = "tmp/data/CIFAR-10/"
+cifar10.maybe_download_and_extract()
+
+class_names = cifar10.load_class_names()
+print(class_names)
+
+# Load the training set
+images_train, cls_train, labels_train = cifar10.load_training_data()
+
+# Load the test set
+images_test, cls_test, labels_test = cifar10.load_test_data()
+print("Size of: ")
+print("-Training-set:\t\t{}".format(len(images_train)))
+print("-Test-set:\t\t{}".format(len(images_test)))
+
+from cifar10 import img_size, num_channels, num_classes
+img_size_cropped = 24
