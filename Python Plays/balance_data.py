1340f7920678e9163a362d4cc15fab20f7e821af
AngryCracker <ads6may@gmail.com>
2017-06-30 12:19:51 +0530

    balancing the data

--- Python Plays/balance_data.py
+++ Python Plays/balance_data.py
@@ -6,11 +6,37 @@ import cv2
 
 train_data = np.load('C:\\Users\\aman Deep Singh\\Documents\\Python\\Practical ML\\Python Plays\\training_data.npy')
 
+# Loop to show training data
+# for data in train_data:
+# 	img = data[0]
+# 	choice = data[1]
+# 	cv2.imshow('test', img)
+# 	print(choice)
+# 	if cv2.waitKey(25) & 0xFF == ord('q'):
+# 		cv2.destroyAllWindows()
+# 		break
+
+df = pd.DataFrame(train_data)
+print(df.head())
+print(Counter(df[1].apply(str)))
+# To balance the data, we will truncate the data to match the length of the lowest list (right in this case)
+
+lefts = []
+rights = []
+forwards = []
+
+# We shuffle the data so that the network is not biased for a particular output
+shuffle(train_data)
+
 for data in train_data:
 	img = data[0]
 	choice = data[1]
-	cv2.imshow('test', img)
-	print(choice)
-	if cv2.waitKey(25) & 0xFF == ord('q'):
-		cv2.destroyAllWindows()
-		break
+
+	if choice == [1, 0, 0]:
+		lefts.append([img, choice])
+	elif choice == [0, 1, 0]:
+		forwards.append([img, choice])
+	elif choice == [0, 0, 1]:
+		rights.append([img, choice])
+	else:
+		print("No matches!")
