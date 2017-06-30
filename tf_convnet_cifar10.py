75eeb63d999f70334e6eca1cd7af5de827eb64ce
AngryCracker <ads6may@gmail.com>
2017-07-01 01:56:19 +0530

    Helper function to plot images

--- tf_convnet_cifar10.py
+++ tf_convnet_cifar10.py
@@ -32,3 +32,38 @@ print("-Test-set:\t\t{}".format(len(images_test)))
 
 from cifar10 import img_size, num_channels, num_classes
 img_size_cropped = 24
+
+def plot_images(images, cls_true, cls_pred=None, smooth=True):
+	assert len(images) == len(cls_true) == 9
+	fig, axes = pls.subplots(3, 3)
+	if cls_pred is None:
+		hspace = 0.3
+	else:
+		hspace = 0.6
+	fig.subplots_adjust(hspace=hspace, wspace=0.3)
+	for i, ax in enumerate(axes.flat):
+		if smooth:
+			interpolation = 'spline16'
+		else:
+			interpolation = 'nearest'
+		ax.imshow(images[i, :, :, :], interpolation=interpolation)
+		# Name of the true class
+		cls_true_name = class_names[cls_true[i]]
+
+		# Show true and predicted classes
+		if cls_pred is None:
+			xlabel = "True: {0}".format(cls_true_name)
+		else:
+			cls_pred_name = class_names[cls_pred[i]]
+			xlabel = "True: {0}\nPred: {1}".format(cls_true_name, cls_pred_name)
+		ax.set_xlabel(xlabel)
+		ax.set_xticks([])
+		ax.set_yticks([])
+
+	plt.show()
+
+# Plot a few images to see if it is correct
+images = images_test[0:9]
+cls_true = cls_test[0:9]
+plot_images(images=images, cls_true=cls_true, smooth=False)
+plot_images(images=images, cls_true=cls_true, smooth=True)
