e7784f970900d0e38262dd4767043a619879e513
AngryCracker <ads6may@gmail.com>
2017-07-03 15:50:37 +0530

    Completed subdued alexnet

--- Python Plays/convnets.py
+++ Python Plays/convnets.py
@@ -4,7 +4,7 @@ from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.normalization import local_response_normalization
 from tflearn.layers.estimator import regression
 
-# Alexnet
+# Alexnet with reduced sizes of fully connected layers
 def convnet1(width, height, lr, output=3):
 	network = input_data(shape=[None, width, height, 1], name='input')
 	network = conv_2d(network, 96, 11, strides=4, activation='relu')
@@ -24,4 +24,5 @@ def convnet1(width, height, lr, output=3):
 	network = dropout(network, 0.5)
 	network = fully_connected(network, output, activation='softmax')
 	network = regression(network, optimizer='momentum', loss='categorical_crossentropy', learning_rate=lr, name='targets')
-	model = tflearn.DNN(network, checkpoint_path='C:\\Users\\Aman Deep Singh\\Documents\\Python\\Practical ML\\')
+	model = tflearn.DNN(network, checkpoint_path='C:\\Users\\Aman Deep Singh\\Documents\\Python\\Practical ML\\Python Plays\\Checkpoints\\convnets_convnet1', max_checkpoints=1, tensorboard_verbose=2, tensorboard_dir='log')
+	return model
