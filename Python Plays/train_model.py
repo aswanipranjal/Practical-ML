2134e50d1a2579f3c16fa8807da2e5fc8e43f9fa
AngryCracker <ads6may@gmail.com>
2017-07-04 00:02:40 +0530

    bugfix

--- Python Plays/train_model.py
+++ Python Plays/train_model.py
@@ -11,7 +11,7 @@ model_name = 'pythondrives-{}-{}-{}-epochs.model'.format(lr, 'alexnetv0.01', epo
 
 model = convnet1(width, height, lr)
 
-train_data = np.load('training_data_v2.npy')
+train_data = np.load('C:\\Users\\Aman Deep Singh\\Documents\\Python\\Car CNN\\final_training_data.npy')
 train = train_data[:-500]
 test = train_data[-500:]
 
@@ -21,7 +21,7 @@ Y = [i[1] for i in train]
 test_x = np.array([i[0] for i in test]).reshape(-1, width, height, 1)
 test_y = [i[1] for i in test]
 
-model.fit({'input': X}, {'targets': Y}, n_epochs=epochs, validation_set=({'input': test_x}, {'targets': test_y}), snapshot_step=500, show_metric=True, run_id=model_name)
+model.fit({'input': X}, {'targets': Y}, n_epoch=epochs, validation_set=({'input': test_x}, {'targets': test_y}), snapshot_step=500, show_metric=True, run_id=model_name)
 
 # tensorboard --logdir=foo:C:/Users/Aman Deep Singh/Documents/Python/Practical ML/Python Plays/log
 
