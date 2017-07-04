511d44f956eeebff9cdb2a24e93da922bc387cb8
AngryCracker <ads6may@gmail.com>
2017-07-04 18:23:36 +0530

    Third convnet

--- Python Plays/convnets.py
+++ Python Plays/convnets.py
@@ -8,17 +8,21 @@ from tflearn.layers.estimator import regression
 # Network architecture: input -> conv -> max_pool -> conv -> max_pool -> conv -> conv -> conv -> max_pool -> fc -> fc -> fc(softmax) -> output
 def convnet1(width, height, lr, output=4):
 	network = input_data(shape=[None, width, height, 1], name='input')
+	# Initial convolutioanl layer
 	network = conv_2d(network, 96, 11, strides=4, activation='relu')
 	network = max_pool_2d(network, 3, strides=2)
 	network = local_response_normalization(network)
+	# Secondry convolutional layer
 	network = conv_2d(network, 256, 5, activation='relu')
 	network = max_pool_2d(network, 3, strides=2)
 	network = local_response_normalization(network)
+	# Tertiary deep convolutional layers
 	network = conv_2d(network, 384, 3, activation='relu')
 	network = conv_2d(network, 384, 3, activation='relu')
 	network = conv_2d(network, 256, 3, activation='relu')
 	network = max_pool_2d(network, 3, strides=2)
 	network = local_response_normalization(network)
+	# Fully connected layers
 	network = fully_connected(network, 256, activation='tanh')
 	network = dropout(network, 0.5)
 	network = fully_connected(network, 256, activation='tanh')
@@ -41,3 +45,10 @@ def convnet2(width, height, lr, output=4):
 	network = regression(network, optimizer='momentum', loss='categorical_crossentropy', learning_rate=lr, name='targets')
 	model = tflearn.DNN(network, checkpoint_path='C:\\Users\\Aman Deep Singh\\Documents\\Python\\Practical ML\\Python Plays\\Checkpoints\\convnets_convnet2', max_checkpoints=1, tensorboard_verbose=2, tensorboard_dir='log')
 	return model
+
+def convnet3(width, height, lr, output=4):
+	network = input_data(shape=[None, width, height, 1], name='input')
+	network = conv_2d(network, 96, 11, strides=4, activation='relu')
+	network = max_pool_2d(network, 3, strides=2)
+	network = local_response_normalization(network)
+	network = conv_2d()
