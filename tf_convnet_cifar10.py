5e12ccde41c66521391f3342f9d25a0eedbb8a1c
AngryCracker <ads6may@gmail.com>
2017-07-05 17:34:02 +0530

    Processing image

--- tf_convnet_cifar10.py
+++ tf_convnet_cifar10.py
@@ -80,6 +80,21 @@ y_true_cls = tf.argmax(y_true, dimension=1)
 # Preprocessing the image for feeding into the convolutional neural network
 def process_image(image, training):
 	if training:
+		# Randomly crop the input image
 		image = tf.random_crop(image, size=[img_size_cropped, img_size_cropped, num_channels])
+		# Randomly flip the image horizontally
 		image = tf.image.random_flip_left_right(image)
-		image = tf.image.random_hue()
+		# Randomly adjust hue, contrast and saturation
+		image = tf.image.random_hue(image, max_delta=0.05)
+		image = tf.image.random_contrast(image, lower=0.3, upper=1.0)
+		image = tf.image.random_brightness(image, max_delta=0.2)
+		image = tf.image.random_saturation(image, lower=0.0, upper=2.0)
+
+		# Limit the image pixels between [0, 1] in case of overflow
+		image = tf.minimum(image, 1.0)
+		image = tf.maximum(image, 0.0)
+	else:
+		# Crop the input image around the center so it is the same size as the images that are randomly cropped during training
+		image = tf.image.resize_image_with_crop_or_pad(image, target_height=img_size_cropped, target_width=img_size_cropped)
+
+	return image
