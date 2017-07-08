0b35a550b1a90b944ea43a27848555dc8c8c07df
AngryCracker <ads6may@gmail.com>
2017-07-08 20:45:11 +0530

    minor

--- Python Plays/train_model.py
+++ Python Plays/train_model.py
@@ -11,7 +11,7 @@ epochs = 7
 model_name = 'pythondrives-{}-{}-{}-epochs.model'.format(lr, 'alexnetv0.02', epochs)
 # model_name = 'pythondrives-{}-{}-{}-epochs.model'.format(lr, 'thinalexv0.01', epochs)
 
-model = convnet3(width, height, lr)
+model = convnet1(width, height, lr)
 
 train_data = np.load('C:\\Users\\Aman Deep Singh\\Documents\\Python\\Car CNN\\final_training_data_v2.npy')
 train = train_data[:-500]
