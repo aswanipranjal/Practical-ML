359436ac96a00b3e6e388acbd594057fd50ee7f3
AngryCracker <ads6may@gmail.com>
2017-07-23 15:09:44 +0530

    minor

--- kmeans-algo.py
+++ kmeans-algo.py
@@ -8,3 +8,34 @@ plt.scatter(X[:, 0], X[:, 1], s=150)
 plt.show()
 
 colors = 10*["g", "r", "c", "b", "k"]
+
+class K_Means:
+	def __init__(self, k=2, tol=0.001, max_iter=300):
+		self.k = k
+		self.tol = tol
+		self.max_iter = max_iter
+
+	def fit(self, data):
+		self.centroids = {}
+		for i in range(self.k):
+			self.centroids[i] = data[i]
+
+		for i in rnge(self.max_iter):
+			self.classifications = {}
+
+			for i in range(self.k):
+				self.classifications[i] = []
+
+			for featureset in X:
+				distances = [np.linalg.norm(featureset - self.centroids[centroid]) for centroid in self.centroids]
+				classification = distances.index(min(distances))
+				self.classifications[classification].append(featureset)
+
+			prev_centroids = dict(self.centroids)
+
+			for classification in self.classifications:
+				pass
+				# self.centroids[classification] = np.average(self.classifications[classification], axis=0)
+
+	def predict(self, data):
+		pass
