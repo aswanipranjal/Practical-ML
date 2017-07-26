22482d3043335c6b6daa7170c3796eeb4a990f51
AngryCracker <ads6may@gmail.com>
2017-07-26 19:12:43 +0530

    making a basic rnn for mnist data

--- tf_rnn.py
+++ tf_rnn.py
@@ -2,3 +2,20 @@
 # Traditional neural networks can not recognize sequences or orders
 # RNNs are used for speech data or sequential data
 # LSTM cells are used in RNNs
+# LSTM cell has a keep/forget gate, sees wha it wants to add from the input and decides what to output
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.python.ops import rnn, rnn_cell
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+hm_epochs = 10
+n_classes = 10
+batch_size = 128
+
+x = tf.placeholder('float', [None, 784])
+y = tf.placeholder('float')
+
+def neural_network_model(data):
+	# Use truncated normal with standard deviation of 0.1 or 0.01 for better results
+	hidden_1_layer = {'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])), 'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+	hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_no]))}
