1293e9aef7236cb2cb4ab2d82b7cbc170cc427a6
AngryCracker <ads6may@gmail.com>
2017-07-26 19:40:20 +0530

    training function

--- tf_rnn.py
+++ tf_rnn.py
@@ -30,3 +30,26 @@ def recurrent_neural_network(x):
 	outputs, states = rnn.rnn(lstm_cell, x, dtype=tf.float32)
 	output = tf.matmul(outputs[-1], layer['weights']) + layer['biases']
 	return output
+
+def train_neural_network(x):
+	prediction = recurrent_neural_network(x)
+	# check function arguments
+	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=mnist.train.labels))
+	optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+
+		for epoch in range(hm_epochs):
+			epoch_loss = 0
+			for _ in range(int(mnist.train.num_examples/batch_size)):
+				epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+				epoch_x = epoch_x.reshape((batch_size, n_chunks, chunk_size))
+				_, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y:epoch_y})
+				epoch_loss += c
+
+			print('Epoch ', epoch, ' completed out of', hm_epochs, '. Loss: ', epoch_loss)
+
+		correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+		accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+		print('Accuracy: ', accuracy.eval({x: mnist.test.images.reshape((-1, n_chunks, chunk_size)), y: mnist.test.labels}))
